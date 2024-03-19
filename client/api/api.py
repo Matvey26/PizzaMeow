00515ee3e94b668e6b4087280d5de37cd3693e3e
sergey162 <usov.serzh.04@mail.ru>
2024-03-19 19:36:07 +0300

    .

--- client/api/api.py
+++ client/api/api.py
@@ -1,10 +1,18 @@
 """Класс текущей сессии, который предоставляет возможность общения """
 
+import config
+import requests
 
 class Session:
     def __init__(self):
-        # Тут надо попробовать подгрузить пароль из кэша (если он сохранён конечно)
-        pass
+        if config.user_pass != None and config.user_login != None and config.user_id != None:
+            params = {'user_id' : config.user_id,
+                      'user_pass' : config.user_pass,
+                      'user_login' : config.user_login}
+            response = requests.get("our server adress", params=params)
+            if response == 200:
+                response.content
+
 
     def sign_up(self, login: str, password: str):
         """Регистрирует нового пользователя"""
