678d5e5a1d219443e49ab4bc424955a13e9e79e1
Matvey26 <matvey.ivchenko@yandex.ru>
2024-04-06 19:18:08 +0300

    Update models.py. Добавил Cart, CartItem. Обновил Order, OrderItems, Pizza, Toppings.

--- server/ServerPizzaMeow/model/models.py
+++ server/ServerPizzaMeow/model/models.py
@@ -37,30 +37,44 @@ class User(Base):
     address = sa.Column(sa.String(400), nullable=True)
 
     orders = relationship('Order', back_populates='user')
+    cart = relationship('Cart', back_populates='user', uselist=False)
     payments = relationship('Payment', back_populates='user')
 
     def __repr__(self):
         return f'<User {self.firstname} {self.lastname}>'
 
 
-class Order(Base):
-    __tablename__ = 'orders'
-
+class OrderCartMixin:
     id = sa.Column(sa.Integer, primary_key=True)
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), nullable=False)
-    status = sa.Column(sa.Enum(StatusEnum), nullable=False)
     total_price = sa.Column(sa.Float, nullable=False)
+
+
+class Order(OrderCartMixin, Base):
+    __tablename__ = 'orders'
+
+    status = sa.Column(sa.Enum(StatusEnum), nullable=False)
 
     user = relationship('User', back_populates='orders')
     order_items = relationship('OrderItem', back_populates='order')
     payment = relationship('Payment', back_populates='order')
 
 
+class Cart(OrderCartMixin, Base):
+    __tablename__ = 'carts'
+
+    user = relationship('User', back_populates='cart')
+    order_items = relationship('OrderItem', back_populates='cart')
+
+
 class OrderItemTopping(Base):
     __tablename__ = 'order_item_toppings'
 
-    order_item_id = sa.Column(sa.Integer, sa.ForeignKey(
-        'order_items.id'), primary_key=True)
+    order_item_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('order_items.id'),
+        primary_key=True
+    )
     topping_id = sa.Column(
         sa.Integer,
         sa.ForeignKey('toppings.id'),
@@ -68,19 +82,37 @@ class OrderItemTopping(Base):
     )
 
 
+class CartItemTopping(Base):
+    __tablename__ = 'cart_item_toppings'
+
+    order_item_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('cart_items.id'),
+        primary_key=True
+    )
+    topping_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('toppings.id'),
+        primary_key=True
+    )
+
+
+class OrderCartItemMixin:
+    id = sa.Column(sa.Integer, primary_key=True)
+    pizza_id = sa.Column(sa.Integer, sa.ForeignKey('Pizza'), nullable=False)
+    total_price = sa.Column(sa.Float, nullable=False)
+    size = sa.Column(sa.Enum(PizzaSizeEnum))
+    quantity = sa.Column(sa.Integer)
+
+
 class OrderItem(Base):
     __tablename__ = 'order_items'
 
-    id = sa.Column(sa.Integer, primary_key=True)
     order_id = sa.Column(
         sa.Integer,
         sa.ForeignKey('orders.id'),
         nullable=False
     )
-    pizza_id = sa.Column(sa.Integer, sa.ForeignKey('Pizza'), nullable=False)
-    total_price = sa.Column(sa.Float, nullable=False)
-    size = sa.Column(sa.Enum(PizzaSizeEnum))
-    quantity = sa.Column(sa.Integer)
 
     order = relationship('Order', back_populates='order_items')
     pizza = relationship('Pizza', back_populates='_order_items')
@@ -88,6 +120,24 @@ class OrderItem(Base):
         argument='Topping',
         secondary='order_item_toppings',
         back_populates='_order_items'
+    )
+
+
+class CartItem(OrderCartItemMixin, Base):
+    __tablename__ = 'cart_items'
+
+    order_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('carts.id'),
+        nullable=False
+    )
+
+    cart = relationship('Cart', back_populates='cart_items')
+    pizza = relationship('Pizza', back_populates='_cart_items')
+    toppings = relationship(
+        argument='Topping',
+        secondary='cart_item_toppings',
+        back_populates='_cart_items'
     )
 
 
@@ -106,6 +156,13 @@ class Topping(Base):
         back_populates='toppings'
     )
 
+    # Не следует использовать этот атрибут вне класса
+    _cart_items = relationship(
+        argument='CartItem',
+        secondary='cart_item_toppings',
+        back_populates='toppings'
+    )
+
 
 class Pizza(Base):
     __tablename__ = 'pizzas'
@@ -117,6 +174,7 @@ class Pizza(Base):
 
     # Не следует использовать этот атрибут вне класса
     _order_items = relationship('OrderItem', back_populates='pizza')
+    _cart_items = relationship('CartItem', back_populates='pizza')
 
 
 class Payment(Base):
