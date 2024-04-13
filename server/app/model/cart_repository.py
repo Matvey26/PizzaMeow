36bff9d5a3f8ff464156e2b0f4aaaa947571ba46
sergey162 <usov.serzh.04@mail.ru>
2024-04-13 21:24:20 +0300

    remake

--- server/app/model/cart_repository.py
+++ server/app/model/cart_repository.py
@@ -7,28 +7,31 @@ class CartRepository(Repository):
         Repository.__init__(self, Cart)
 
     def create_cart(self, user : User):
-        new_cart = Cart(user=user, order_items=[])
+        new_cart = Cart(user_id=user.id)
         self.session.add(new_cart)
         self.session.commit(new_cart)
 
     def get_cart(self, user : User):
-        return self.session.query(Cart).filter_by(user=self.user).first()
+        return self.session.query(Cart).filter_by(user=user).first()
 
     def delete_cart(self, cart : Cart):
         deleted = self.session.delete(cart)
         self.session.commit()
+        return deleted
 
-    def add_item(self, cart : Cart, item : CartItem):
-        cart.order_items.append(item)
+    def add_item(self, cart : Cart, *items : CartItem):
+        for item in items:
+            cart.order_items.append(item)
         self.session.commit(cart)
 
-    def delete_item(self, cart : Cart, item : CartItem):
-        cart.order_items.remove(item)
+    def delete_item(self, cart : Cart, *items : CartItem):
+        for item in items:
+            cart.order_items.remove(item)
         self.session.commit(cart)
 
     def clear_cart(self, cart : Cart):
         cart.order_items.clear()
         self.session.commit(cart)
 
-    def get_items(self, Cart : cart):
+    def get_items(self, cart : Cart):
         return cart.order_items
