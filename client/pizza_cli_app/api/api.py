a70a0eca4d776e15f1b48d1e4b6238b607031794
Matvey26 <matvey.ivchenko@yandex.ru>
2024-05-05 13:15:07 +0300

    Create mock_data.py, Update api/, commands/. Добавлены мок-данные. Улучшен вывод корзины

--- client/pizza_cli_app/api/api.py
+++ client/pizza_cli_app/api/api.py
@@ -6,35 +6,8 @@ import pathlib
 import json
 url = 'http://127.0.0.1:8000/api/'
 
-pizzas = [
-    {"id": 1, "name": "Margherita", "description": "Tomato, mozzarella, and basil.", "price": 8.0},
-    {"id": 2, "name": "Pepperoni", "description": "Tomato, mozzarella, and pepperoni.", "price": 10.0},
-    {"id": 3, "name": "BBQ Chicken", "description": "BBQ sauce, chicken, red onions, and cilantro.", "price": 12.0},
-    {"id": 4, "name": "Veggie", "price": 11.0},
-    {"id": 5, "name": "Hawaiian", "description": "Tomato, mozzarella, ham, and pineapple.", "price": 10.5},
-    {"id": 6, "name": "Meat Lovers", "description": "Tomato, mozzarella, pepperoni, sausage, ham, and bacon.", "price": 13.0},
-    {"id": 7, "name": "Margherita Deluxe", "description": "Tomato, mozzarella, cherry tomatoes, basil, and garlic.", "price": 9.0},
-    {"id": 8, "name": "Buffalo Chicken", "description": "Buffalo sauce, chicken, ranch, and red onions.", "price": 12.5},
-    {"id": 9, "name": "Cheese", "price": 7.0},
-    {"id": 10, "name": "Supreme", "description": "Tomato, mozzarella, pepperoni, sausage, mushrooms, onions, and olives.", "price": 14.0},
-    {"id": 11, "name": "Garden", "description": "Tomato, mozzarella, spinach, artichokes, and olives.", "price": 11.5},
-    {"id": 12, "name": "Sausage", "description": "Tomato, mozzarella, and sausage.", "price": 10.5},
-    {"id": 13, "name": "Bacon", "description": "Tomato, mozzarella, and bacon.", "price": 11.0},
-    {"id": 14, "name": "Mushroom", "description": "Tomato, mozzarella, and mushrooms.", "price": 10.0},
-    {"id": 15, "name": "Onion", "description": "Tomato, mozzarella, and onions.", "price": 9.5},
-    {"id": 16, "name": "Black Olive", "description": "Tomato, mozzarella, and black olives.", "price": 9.5},
-    {"id": 17, "name": "White", "description": "Garlic, mozzarella, and ricotta.", "price": 10.0},
-    {"id": 18, "name": "Spinach", "description": "Garlic, mozzarella, spinach, and artichokes.", "price": 11.5},
-    {"id": 19, "name": "Chicken Alfredo", "description": "Alfredo sauce, chicken, and mozzarella.", "price": 12.0},
-    {"id": 20, "name": "Pesto", "description": "Pesto sauce, mozzarella, and cherry tomatoes.", "price": 11.0},
-    {"id": 21, "name": "Taco", "description": "Taco sauce, ground beef, lettuce, and tomatoes.", "price": 11.5},
-    {"id": 22, "name": "Seafood", "description": "Tomato, mozzarella, shrimp, calamari, and clams.", "price": 14.0},
-    {"id": 23, "name": "Caprese", "description": "Tomato, mozzarella, basil, and balsamic glaze.", "price": 11.0},
-    {"id": 24, "name": "Greek", "description": "Tomato, mozzarella, feta, kalamata olives, and red onions.", "price": 12.0},
-    {"id": 25, "name": "Buffalo Cauliflower", "description": "Buffalo sauce, cauliflower, and ranch.", "price": 12.5}
-]
 
-
+from .mock_data import *
 
 def connection_error_handler(func):
     def wrapper(*args, **kwargs):
@@ -247,13 +220,34 @@ class Session:
         response = requests.get(url + f'pizzas/{id}')
         if response.status_code == 200:
             return response.json()
-        return response.status_code
+        return (response.status_code, response.json()['detail'])
 
     # ------------------ РАБОТА С КОРЗИНОЙ ПОЛЬЗОВАТЕЛЯ -----------------
 
     @connection_error_handler
     def get_cart_items(self):
-        """Получить содержимое своей корзины (без пагинации, так как размер корзины заведомо небольшой)"""
+        """Получить содержимое своей корзины (без пагинации, так как размер корзины заведомо небольшой)
+        Корзина представляет собой словарь вида
+
+        {
+            'id': int,  # id корзины
+            'total_price': float,  # цена всей корзины
+            'cart_items': list
+        }
+
+        где поле cart_items это список словарей объектов корзины вида
+
+        {
+            'id': int,  # уникальный ID элемента корзины
+            'total_price': float  # цена за объект корзины (с учётом количества, размера, теста)
+            'pizza_id': int,
+            'pizza_name': str,
+            'quantity': int,
+            'size': str,  # а точнее enum: [small, medium, large]
+            'dough': str  # а точнее enum: [thin, classic]
+        }
+        """
+        return cart
         headers = {'Authorization': f'Bearer {self.__token}'}
         response = requests.get(url + 'carts', headers=headers)
         if response.status_code == 200:
--- client/pizza_cli_app/commands/showcart.py
+++ client/pizza_cli_app/commands/showcart.py
@@ -6,14 +6,20 @@ class ShowCart(Base):
     def run(self, session : Session):
         show_id = self.options.show_id
         answer = session.get_cart_items()
-        if isinstance(answer, str):
-            print(f'{answer} Попробуйте снова')
+
+        if isinstance(answer, tuple):
+            print(answer[1])
             return
-        for item in answer:
+        
+        print('Корзина:')
+        print(f"Цена корзины: {answer['total_price']}")
+        for item in answer['cart_items']:
+            print('--------------------------------')
             if show_id:
-                print(f"item id - {item['id']}")
-            print(f"name pizza - {item['pizza_name']}")
-            print(f"price - {item['price']}")
-            print(f"quantity - {item['quantity']}")
-            print(f"size - {item['size']}")
-            print(f"dough - {item['dough']}\n")
+                print(f"item id: {item['id']}")
+            print(f"Цена - {item['total_price']}")
+            print(f"Название пиццы - {item['pizza_name']}")
+            print(f"Количество - {item['quantity']}")
+            print(f"Размер пиццы - {item['size']}")
+            print(f"Тип теста - {item['dough']}\n")
+        print('--------------------------------')
